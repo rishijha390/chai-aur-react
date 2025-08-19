85da349225efe7bfca2583d87afcca44e57d9723
rishijha390 <rishijha390@gmail.com>
2025-08-20 00:05:07 +0530

    Added 03tailwindprops as normal folder

---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function Card() {
+  return (
+    
+     <div className="flex flex-col rounded-xl  p-4"
+        style={{
+          border: '0.88px solid',
+  
+          backdropFilter: 'saturate(180%) blur(14px)',
+          background: ' #ffffff0d',
+        }}
+      >
+        <div>
+          <img
+            src="https://images.pexels.com/photos/33362180/pexels-photo-33362180.jpeg"
+            alt="nft-gif"
+            width="400"
+            height="400"
+            className="rounded-xl"
+          />
+        </div>
+        <div className="flex flex-col  rounded-b-xl py-4 ">
+          <div className="flex justify-between">
+            <h1 className="font-RubikBold ">Bored ape nft accidental</h1>
+            <h1 className="font-bold font-RubikBold">Price</h1>
+          </div>
+          <div className="flex  justify-between font-mono">
+            <p>#345</p>
+            <p>0.01</p>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Card
