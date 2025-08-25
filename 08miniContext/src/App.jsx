620073677ced298d88c925eeb21bd7f11a13b81f
rishijha390 <rishijha390@gmail.com>
2025-08-26 00:49:56 +0530

    Added new folder with mini context project update

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-         <h1>Mini context with hitesh sir</h1>
-    </>
+    <UserContextProvider>
+   <h1>React with hitesh sir</h1>
+   <Login />
+   <Profile />
+   </UserContextProvider>
   )
 }
 
