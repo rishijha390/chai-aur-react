4052b423476bd4571a81e8acb730e9fbc4d754f4
rishijha390 <rishijha390@gmail.com>
2025-09-03 00:06:16 +0530

    Added new folder with Mega blog  project update 03-09-2025

--- 12MegaBlog/src/appwrite/Config.js
+++ 12MegaBlog/src/appwrite/Config.js
@@ -106,6 +106,25 @@ export class Service{
                 return false
         }
       }
+      async deleteFile(fileId){
+        try{
+           await this.bucket.deleteFile(
+            conf.appwriteBucketId,
+            fileId
+           )
+           return true
+        }catch (error){
+            console.log(`Appwrite service :: deleteFile :: 
+                error`,error);
+                return false
+        }
+      }
+      getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+      }
 }
 
 
