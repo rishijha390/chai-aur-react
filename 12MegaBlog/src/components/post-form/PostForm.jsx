6a5a92dd0759877570f0943dfbd9f4fb6f243ab2
rishijha390 <rishijha390@gmail.com>
2025-09-06 23:58:59 +0530

    added rte file

---
+++ 12MegaBlog/src/components/post-form/PostForm.jsx
@@ -0,0 +1,13 @@
+import React,{useCallback} from 'react'
+import {useForm} from 'react-hook-form'
+import {Button,Input,Select,RTE} from '../index'
+import app
+
+
+function PostForm() {
+  return (
+    <div>PostForm</div>
+  )
+}
+
+export default PostForm
